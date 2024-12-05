92bf2edd1d3bd80ebe3ff4327b2225fd0f82360b
Aaleeyah Ivy-Kilgore <53322518+AaleeyahIvy@users.noreply.github.com>
2024-12-05 14:52:28 -0500

    update resume page blank

--- src/components/base-components/MyResumeComponent.vue
+++ src/components/base-components/MyResumeComponent.vue
@@ -1,16 +1,11 @@
 <template>
     <div>
         <p>{{ aboutpage }}</p>
-        <pdf src="../../assets/files/Resume.docx (1).pdf"></pdf>
     </div>
 </template>
 <script>    
-import pdf from 'vue-pdf';
 export default {
     name: 'MyResumeComponent',
-    components: {
-        pdf
-    },
     data() {
         return {
             aboutpage: 'This is the Resume page'
